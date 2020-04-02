15e48631c260cf16a184d794bd8ec869229d62d0
aasll <aslidogan97@gmail.com>
2020-04-02 15:43:21 +0300

    player Movement editing

--- Hit The Goalkeeper/Assets/Scripts/test/PlayerMovement.cs
+++ Hit The Goalkeeper/Assets/Scripts/test/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float comerValue;
+   // public float comerValue;
+   // public float a;
     public float rise;
     public GameObject goalkeeper;
     public Rigidbody rb;
@@ -13,13 +14,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int shootPower; //Lerp için time değişkeni inputtan gelen değer.
 
     [SerializeField] public Animator cameraAnim;
-
-    public float a;
-
+   Vector3 position;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        a = Random.Range(0, 3);
+        //a = Random.Range(0, 3);
     }
 
     // Update is called once per frame
@@ -28,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
         if (!GameManager.main.ballAnimStartTrigger) return;
         //If ball is kicked by the player, then we will the ball to corresponding position.
         shootPower = GameManager.main.ballShootPowerValue;
+       
         Movement();
     }
 
@@ -37,30 +37,31 @@ public class PlayerMovement : MonoBehaviour
         //  Vector3 firstPos = transform.position;      
         //   rb.AddForce(direction * comerValue,ForceMode.Force);
 
-
+    position = GameManager.main.transformPositionToShoot.position;
+       
+          Debug.Log(position.y +" "+ position.z);
         transform.position =
             Vector3.Lerp(transform.position, goalkeeper.transform.position, shootPower * Time.deltaTime);
         //   transform.position= Vector3.MoveTowards(transform.localPosition,goalkeeper.transform.localPosition,comerValue*Time.deltaTime);
-        if (this.gameObject.transform.position.z < goalkeeper.transform.localPosition.z / 2)
+        if (this.gameObject.transform.position.z < goalkeeper.transform.localPosition.z/2 )
         {
-            this.gameObject.transform.position += new Vector3(0, rise, transform.localPosition.z) * Time.deltaTime;
+            this.gameObject.transform.position -= new Vector3(position.x, position.y,position.z) * Time.deltaTime;
             //  this.gameObject.transform.localPosition += new Vector3 (transform.localPosition.x, rise, 0)*Time.deltaTime;
         }
-        else if (this.gameObject.transform.position.z >= goalkeeper.transform.localPosition.z / 2)
-        {
-            var position = GameManager.main.transformPositionToShoot.position;
-            this.gameObject.transform.localPosition += new Vector3(position.x, position.y, transform.localPosition.z) * Time.deltaTime;
-            //  transform.position= Vector3.Lerp( transform.position,goalkeeper.transform.position,Time.deltaTime*rise);
-        }
+        // else if (this.gameObject.transform.position.z >= goalkeeper.transform.localPosition.z / 2)
+        // {
+        //     this.gameObject.transform.localPosition -= new Vector3(position.x,0, position.z) * Time.deltaTime;
+        //     //  transform.position= Vector3.Lerp( transform.position,goalkeeper.transform.position,Time.deltaTime*rise);
+        // }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!other.CompareTag("goalkeeper")) return;
-        cameraAnim.SetBool("goalKeeperTouch", true);
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (!other.CompareTag("goalkeeper")) return;
+    //     cameraAnim.SetBool("goalKeeperTouch", true);
 
-        final = true;
-        direction = false;
-        speed = false;
-    }
+    //     final = true;
+    //     direction = false;
+    //     speed = false;
+    // }
 }
